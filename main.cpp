9a113003286bd650769fa2ee2d7b912960d30856
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-15 00:57:00 +0200

    minor commit

--- main.cpp
+++ main.cpp
@@ -3,21 +3,8 @@
 
 using namespace px;
 
-struct DebugComponent : public IComponent
-{
-    float x, y;
-    DebugComponent() : IComponent() { }
-};
-
 int main()
 {
-    DebugComponent test = DebugComponent();
-    test.init(3);
-    test.x = 60;
-
-    cout << test.ID << endl;
-    cout << test.x << endl;
-    
     //...
 
     return 0;
