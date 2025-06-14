f531b8916bf74beb23a0eb6ce6ce4f638fd55f58
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-15 00:11:21 +0200

    components

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "pixelcore/colors.h"
+#include "pixelcore/ECS.h"
+
 using namespace px;
 
-#include <iostream>
-using std::cout, std::endl;
+struct DebugComponent : public IComponent
+{
+    float x, y;
+    DebugComponent() : IComponent() { }
+};
 
 int main()
 {
-    Color color = pack(0, 0, 137);
-    RGBA rgba = unpack(color);
-    nRGBA normalized = nunpack(color);
+    DebugComponent test = DebugComponent();
+    test.init(3);
+    test.x = 60;
 
-    cout << color << endl;
-    cout << rgba << endl;
-    cout << normalized << endl;
-    cout << hex(color) << endl;
-    cout << hex(rgba) << endl;
-    cout << hex(normalized) << endl;
+    cout << test.ID << endl;
+    cout << test.x << endl;
+    
+    //...
 
     return 0;
 }
