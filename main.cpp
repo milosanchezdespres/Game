3e05c685f6db7f3fb65f6463b03abf38c74f45d2
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-17 13:06:20 +0200

    minor commit

--- main.cpp
+++ main.cpp
@@ -15,12 +15,15 @@ int main()
 
     test.add<Test>({COLOR("#b04830")});
 
+    auto color = test.component<Test>()->color;
     auto rgba = RGBA(test.component<Test>()->color);
     auto normalized = NRGBA(test.component<Test>()->color);
 
-    std::cout << test.component<Test>()->color << std::endl;
-    std::cout << rgba << std::endl;
+    std::cout << color      << std::endl;
+    std::cout << rgba       << std::endl;
     std::cout << normalized << std::endl;
+
+    std::cout << std::endl;
 
     return 0;
 }
