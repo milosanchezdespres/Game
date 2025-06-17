2083c3a50623ec2dc33f7b74965d3e971a7f02d4
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-17 23:54:22 +0200

    unit test blit function (result = bouncing image)

--- MyGame.h
+++ MyGame.h
@@ -28,9 +28,21 @@ struct MyGame : public IGameLoop
         //print(FPS);
     }
 
+    int axis_x = 1, axis_y = 1;
+    float x, y;
+
     void render() override
     {
-        SCREEN().blit(texture("default"), 50, 50, 2);
+        x += axis_x * (250 * DELTA);
+        y += axis_y * (250 * DELTA);
+
+        if(x > (SCREEN().width - texture("default")->width())) axis_x = -1;
+        if(x < - texture("default")->width()) axis_x = 1;
+
+        if(y > (SCREEN().height - texture("default")->height())) axis_y = -1;
+        if(y <  - texture("default")->height()) axis_y = 1;
+
+        SCREEN().blit(texture("default"), x, y, 2);
         SCREEN().blit(texture("default"), 150, 50, 2);
         SCREEN().blit(texture("default"), 150, 150, 2);
         SCREEN().blit(texture("default"), 50, 150, 2);
