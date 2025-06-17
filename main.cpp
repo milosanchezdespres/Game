b36ea90cef3bb1244c8d11ba3944cde28c357066
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-17 20:57:31 +0200

    unit testing ECS display and texture all together

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "pixelcore/Colors.h"
+#include "pixelcore/ECS.h"
 #include "pixelcore/Display.h"
 using namespace px;
 
 #include <iostream>
+
+struct TestComponent
+{
+    Texture* texture;
+    float x, y;
+    int scale;
+    Surface surface;
+};
+
+//unit test to see if everything works alright
+//next step : proper game manager
 
 int main()
 {
@@ -10,13 +22,18 @@ int main()
 
     auto bgcolor = NCOLOR("#091436");
 
-    Texture* test = Texture().load("assets/default");
+    Entity test = MAKE_ENTITY;
+    test.add<TestComponent>({Texture().load("assets/default"), 50, 50, 4, {0, 0, 32, 32}});
 
     while(SCREEN().active())
     {
         SCREEN().begin_render(EXPAND(bgcolor));
 
-        SCREEN().blit(test, 50, 50, 3);
+        auto test_component = test.component<TestComponent>();
+        SCREEN().blit(test_component->texture, 
+            test_component->x, test_component->y, 
+            test_component->scale, 
+            test_component->surface);
 
         SCREEN().end_render();
     }
