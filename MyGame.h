716d5517bf5da733e69d2273f7ef54f4cfa69303
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-18 02:19:20 +0200

    minor commit

--- MyGame.h
+++ MyGame.h
@@ -5,6 +5,11 @@
 #include <numeric>
 
 //this mess is just debugging font & fps
+int axis_x = 1, axis_y = 1;
+float x = 0, y = 0;
+
+std::deque<float> fps_values;
+static constexpr size_t fps_buffer_size = 10;
 
 struct MyGame : public IGameLoop
 {
@@ -24,17 +29,6 @@ struct MyGame : public IGameLoop
 
     void update() override
     {
-        //...
-    }
-
-    int axis_x = 1, axis_y = 1;
-    float x = 0, y = 0;
-
-    std::deque<float> fps_values;
-    static constexpr size_t fps_buffer_size = 10;
-
-    void render() override
-    {
         x += axis_x * (250 * DELTA);
         y += axis_y * (250 * DELTA);
 
@@ -44,17 +38,18 @@ struct MyGame : public IGameLoop
         if(y > (SCREEN().height - texture("default")->height())) axis_y = -1;
         if(y < -texture("default")->height()) axis_y = 1;
 
-        SCREEN().blit(texture("default"), x, y, 2);
-
         fps_values.push_back(FPS);
         if (fps_values.size() > fps_buffer_size) fps_values.pop_front();
 
         float avg_fps = std::accumulate(fps_values.begin(), fps_values.end(), 0.0f) / fps_values.size();
+    }
+
+    void render() override
+    {
+        SCREEN().blit(texture("default"), x, y, 2);
 
         FONT->write(std::to_string(static_cast<int>(avg_fps)).c_str(), 25, 25, 4);
         FONT->write("hello world", 100, 100, 4);
-
-        //...
     }
 
     void exit() override
