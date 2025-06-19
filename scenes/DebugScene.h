9671f48eaf9cf0a2515f9a7a09238913e924ecdc
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-20 00:59:53 +0200

    debugging inputs

--- scenes/DebugScene.h
+++ scenes/DebugScene.h
@@ -5,6 +5,7 @@
 struct DebugScene : public IScene
 {
     struct Pos { float x, y; };
+    struct Velocity { float x, y; };
     struct Sprite { tx::view texture; };
 
     tx::view default_texture;
@@ -19,13 +20,17 @@ struct DebugScene : public IScene
         entity = 
         {
             Pos{50, 50},
+            Velocity{300, 300},
             Sprite{{default_texture, {16, 16, 32, 32}}}
         };
     }
 
     void update() override
     {
-        //...
+        if(HOLD(Z) || HOLD(W)) entity.component<Pos>().y -= entity.component<Velocity>().x * screen::delta;
+        if(HOLD(S)) entity.component<Pos>().y += entity.component<Velocity>().x * screen::delta;
+        if(HOLD(A) || HOLD(Q)) entity.component<Pos>().x -= entity.component<Velocity>().x * screen::delta;
+        if(HOLD(D)) entity.component<Pos>().x += entity.component<Velocity>().x * screen::delta;
     }
 
     void render() override
