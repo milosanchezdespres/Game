164c0168d0ac82368662d570edc84069754e39bf
milosanchezdespres <milosanchezdespres@gmail.com>
2025-06-20 00:27:29 +0200

    minor commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "pixelcore/pixelcore.h"
 
 #include "scenes/DebugScene.h"
+//...
 
 int main()
 {
     board::upload<DebugScene>("debug");
+    //...
 
     return board::run("untitled", 800, 600, "debug");
 }
